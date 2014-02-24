432dafc9e01575b0f0283088caf5f89166120edf
fcecin <fcecin@gmail.com>
2014-02-24 17:59:03 -0300

    hello world sem UI fazendo ping-pong no server automaticamente

---
+++ src/sneerteam/android/chat/LoopingHandlerRunnable.java
@@ -0,0 +1,43 @@
+package sneerteam.android.chat;
+
+import android.os.Handler;
+
+/*
+ * A Runnable given to an Android Handler that sets itself up for being called again
+ *   until someone tells it to stop doing that. 
+ *   
+ * Intervals are in milliseconds.
+ * 
+ * On the constructor this actually posts the runnable to run after the specified interval. 
+ */
+public class LoopingHandlerRunnable implements Runnable {
+
+	Handler handler;
+	Runnable inner;
+	int interval;
+	boolean stopped = false;
+	
+	// remove a crap
+	LoopingHandlerRunnable(Runnable inner, int interval) {
+		this.handler = new Handler();
+		this.inner = inner;
+		this.interval = interval;
+		handler.postDelayed(this, interval);
+	}
+	
+	public void stop() {
+		stopped = true;
+	}
+	
+	public boolean isStopped() {
+		return stopped;
+	}
+
+	@Override
+	public void run() {
+		if (! stopped) {
+			inner.run();
+			handler.postDelayed(this, interval);
+		}
+	}
+}
