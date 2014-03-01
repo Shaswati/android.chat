3113984fd8a8bd4e4cd3ff7a248b2b280d01658f
Altieres <altiereslopes@webgoal.com.br>
2014-03-01 17:28:42 -0300

    speech arrow layout refactoring

--- src/sneerteam/android/chat/ui/TriangleDrawable.java
+++ src/sneerteam/android/chat/ui/TriangleDrawable.java
@@ -1,52 +1,43 @@
 package sneerteam.android.chat.ui;
 
-import android.graphics.Canvas;
 import android.graphics.ColorFilter;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.drawable.Drawable;
 
-public class TriangleDrawable extends Drawable {
+public abstract class TriangleDrawable extends Drawable {
 
-	private Paint paint;
+	protected Path path;
+	protected Paint paint;
 
 	public TriangleDrawable(int color) {
+		path = new Path();
 		paint = new Paint();
-		paint.setAntiAlias(true);
 		paint.setColor(color);
-		paint.setStyle(Paint.Style.STROKE);
+		paint.setStyle(Paint.Style.FILL);
 	}
 
 	@Override
-	public void draw(Canvas canvas) {
-		int height = getBounds().height();
-		int width = getBounds().width();
+	public void setAlpha(int alpha) {
+	
+	}
+	
+	@Override
+	public void setColorFilter(ColorFilter cf) {
+	
+	}
+	
+	@Override
+	public int getOpacity() {
+	  return 255;
+	}
+	
+	protected int width() {
+		return getBounds().width();
+	}
 
-		Path path = new Path();
-		paint.setStyle(Paint.Style.FILL);
-		
-		path.moveTo(0, 0);
-		path.lineTo(width, 0);
-		path.lineTo(width, height);
-		path.lineTo(0, 0);
-		
-		path.close();
-		canvas.drawPath(path, paint);
-  }
-
-  @Override
-  public void setAlpha(int alpha) {
-
-  }
-
-  @Override
-  public void setColorFilter(ColorFilter cf) {
-
-  }
-
-  @Override
-  public int getOpacity() {
-    return 255;
-  }
-
+	protected int height() {
+		return getBounds().height();
+	}
+	
 } 
