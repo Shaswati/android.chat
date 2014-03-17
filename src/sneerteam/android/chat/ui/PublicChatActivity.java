865cc63497276148f47216d87d0f6ce74299b05d
Rodrigo B. de Oliveira <rbo@acm.org>
2014-03-16 23:39:26 -0300

    snapi based chat skeleton (wip)

--- src/sneerteam/android/chat/ui/PublicChatActivity.java
+++ src/sneerteam/android/chat/ui/PublicChatActivity.java
@@ -1,89 +1,166 @@
 package sneerteam.android.chat.ui;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import sneerteam.android.chat.ChatClient;
-import sneerteam.android.chat.ChatListener;
 import sneerteam.android.chat.Message;
 import sneerteam.android.chat.R;
-import android.annotation.SuppressLint;
+import sneerteam.api.ICloud;
+import sneerteam.api.ISubscriber;
+import sneerteam.api.ISubscription;
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.StrictMode;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.RemoteException;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+@TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
 public class PublicChatActivity extends Activity {
 	
+	volatile ICloud cloud;
+	volatile ISubscription subscription;
+	
+	final ServiceConnection snapi = new ServiceConnection() {
+		
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder binder) {
+			toast("connected");
+			cloud = ICloud.Stub.asInterface(binder);
+			try {
+				subscription = cloud.sub(chatUri(), subscriber);
+			} catch (RemoteException e) {
+			}
+		}
+		
+		@Override
+		public void onServiceDisconnected(ComponentName name) {
+			toast("disconnected");
+			subscription = null;
+			cloud = null;
+		}
+	};
+	
+	final ISubscriber subscriber = new ISubscriber.Stub() {
+		@Override
+		public void on(Uri path, Bundle value) throws RemoteException {
+			long timestamp = value.getLong("timestamp");
+			String sender = value.getString("sender");
+			String contents = value.getString("contents");
+			Message chatMessage = new Message(timestamp, sender, contents);
+			
+			// the callback will be dispatched in a thread pool thread
+			// so to update the UI we need a handler to get it there 
+			handler.sendMessage(android.os.Message.obtain(handler, ON_CHAT_MESSAGE, chatMessage));
+		}
+	};
+	
+	private static final int ON_CHAT_MESSAGE = 1;
+	
+	final Handler handler = new Handler() {
+  	@Override
+  	public void handleMessage(android.os.Message m) {
+  		switch (m.what) {
+  		case ON_CHAT_MESSAGE:
+  			onMessage((Message)m.obj);
+  			break;
+  			default:
+  				super.handleMessage(m);
+  		}
+  	}		
+  };
+	
 	private static List<Message> MESSAGES = initMessages();
-
-	private ChatClient chat;
+	
 	private String myNick = null;
 	
 	private ChatAdapter chatAdapter;
 
-	@SuppressLint("NewApi")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		// ------------------------------------------------------------
-        // Remove the retarded "must run networking on ANOTHER thread"
-        //   limitation imposed by Android.
-        // We know what we're doing and adding another thread will NOT 
-        //   help us in any way, shape or form.
-        // ------------------------------------------------------------
-
-        if (android.os.Build.VERSION.SDK_INT > 9) {
-    		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-    		StrictMode.setThreadPolicy(policy);
-    	}
-        
-        myNick = preferences().getString("myNick", null);
-        
-		setContentView(R.layout.activity_chat);
+		myNick = preferences().getString("myNick", null);
 		
+		setContentView(R.layout.activity_chat);
 		setTitle("Public Chat");
 		
 		ListView listView = (ListView) findViewById(R.id.listView);
 		chatAdapter = new ChatAdapter(this, R.layout.list_item_user_message, R.layout.list_item_contact_message, MESSAGES);
 		chatAdapter.setSender(myNick);
-        listView.setAdapter(chatAdapter);
-        
-        try {
-			chat = new ChatClient(new ChatListener() { @Override public void on(Message msg) {
-				if (Collections.binarySearch(MESSAGES, msg) < 0) {
-					MESSAGES.add(msg);
-					Collections.sort(MESSAGES);
-					chatAdapter.notifyDataSetChanged();
-				}
-			}});
-		} catch (IOException e) {
-			chatAdapter.add(new Message(System.currentTimeMillis(), "<ERROR>", e.getMessage()));
-		}
+		listView.setAdapter(chatAdapter);
+		
+		connect();
+	}
+
+	private void connect() {
+		bindService(
+				bindCloudServiceIntent(),
+				snapi,
+				Context.BIND_AUTO_CREATE + Context.BIND_ADJUST_WITH_ACTIVITY);
+	}
+
+	private Intent bindCloudServiceIntent() {
+		Intent bindIntent = new Intent("sneerteam.intent.action.BIND_CLOUD_SERVICE");
+		bindIntent.setClassName("sneerteam.snapi", "sneerteam.snapi.CloudService");
+		return bindIntent;
 	}
 
 	@Override
 	protected void onDestroy() {
-		if (chat != null) chat.destroy();
+		unsubscribe();
+		disconnect();
 		super.onDestroy();
+	}
+
+	private void disconnect() {
+		if (cloud != null) {
+			cloud = null;
+			unbindService(snapi);
+		}
+	}
+
+	private void unsubscribe() {
+		if (subscription != null) {
+			try {
+				subscription.dispose();
+			} catch (RemoteException e) {
+				e.printStackTrace();
+			}
+			subscription = null;
+		}
 	}
 	
 	private static List<Message> initMessages() {
-    	List<Message> messages = new ArrayList<Message>();
-    	messages.add(new Message(0, "Sneer", "Welcome to the public chat room. Be awesome."));
-        return messages;
-    }
+		List<Message> messages = new ArrayList<Message>();
+		messages.add(new Message(0, "Sneer", "Welcome to the public chat room. Be awesome."));
+		return messages;
+	}
+	
+	void onMessage(Message msg) {
+		if (Collections.binarySearch(MESSAGES, msg) < 0) {
+			MESSAGES.add(msg);
+			Collections.sort(MESSAGES);
+			chatAdapter.notifyDataSetChanged();
+		}
+	}
 	
 	public void onSendButtonClick(View view) {
 		if (!chatAdapter.hasSender())
@@ -103,7 +180,7 @@ public class PublicChatActivity extends Activity {
 		    .setPositiveButton("OK", new DialogInterface.OnClickListener() { public void onClick(DialogInterface dialog, int whichButton) {
 	        	myNick = input.getText().toString();
 	        	Editor editor = preferences().edit();
-				editor.putString("myNick", myNick);
+	        	editor.putString("myNick", myNick);
 	        	editor.commit();
 	        	chatAdapter.setSender(myNick);
 	        	sendText();
@@ -111,14 +188,41 @@ public class PublicChatActivity extends Activity {
 	}
 	
 	private void sendText() {
+		if (cloud == null)
+			return;
+		
 		TextView widget = (TextView)findViewById(R.id.editText);
-		String whatusay = widget.getText().toString();
+		String text = widget.getText().toString();
+		try {
+			cloud.pub(chatUri(), messageBundleFor(text));
+		} catch (RemoteException e) {
+			toast(e.getMessage());
+			e.printStackTrace();
+			return;
+		}
 		widget.setText("");
-		chat.send(myNick, whatusay);
+	}
+
+	private Bundle messageBundleFor(String text) {
+		Bundle bundle = new Bundle();
+		// TODO: bundle.putParcelable("type", Keyword.for("msg"));
+		bundle.putString("type", ":msg");
+		bundle.putString("sender", myNick);
+		bundle.putString("contents", text);
+		bundle.putLong("timestamp", System.currentTimeMillis());
+		return bundle;
 	}
 	
 	private SharedPreferences preferences() {
 		return PreferenceManager.getDefaultSharedPreferences(this);
+	}
+	
+	void toast(String message) {
+		Toast.makeText(PublicChatActivity.this, message, Toast.LENGTH_SHORT).show();
+	}
+	
+	Uri chatUri() {
+		return Uri.parse("content://public/chat");
 	}
 
 }
