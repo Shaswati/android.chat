57984c5995bc0fc7a3a9282e9e2718ec0e85f3e9
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2014-07-07 01:15:43 -0300

    Minor

--- src/sneer/chat/Party.java
+++ src/sneer/chat/Party.java
@@ -2,7 +2,7 @@ package sneer.chat;
 
 import rx.Observable;
 
-/** An individual or a group. See hierarchy. */
+/** An individual or a group. See type hierarchy. */
 public interface Party {
 
 	Observable<String> publicKey();
@@ -10,7 +10,8 @@ public interface Party {
 	/**	The nickname you give to a party. */
 	Observable<String> nickname();
 
-	/** The name this Party gives itself. */
+	/** The name this Party gives itself.
+	 * JUST AN EXAMPLE FOR THE FUTURE. NOT IMPLEMENTED OR USED IN THE CHAT APP. */
 	Observable<String> name();
 	
 }
